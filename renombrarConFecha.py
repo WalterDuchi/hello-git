be6096d4fa26ab9f69e475f8db4dccd8ddfd1398
Walter Alejandro Duchi Rivera <walter.duchiriv@ug.edu.ec>
2024-02-24 20:28:24 -0500

    corrección en el formato de fecha

--- renombrarConFecha.py
+++ renombrarConFecha.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import exifread
 
 # Directorio de origen
-directorio = r'C:\Users\Walter Duchi\Pictures\prueba'
+directorio = r'C:\Users\Walter Duchi\OneDrive - Universidad de Guayaquil\Fotos Celular'
 
 # Obtener la lista de archivos en el directorio
 archivos = os.listdir(directorio)
@@ -38,13 +38,13 @@ for archivo in archivos:
     fecha_antigua = min(fechas)
     
     # Formatear la fecha como deseas para el nuevo nombre de archivo
-    nuevo_nombre = fecha_antigua.strftime('%m-%d-%Y %H-%M-%S') + os.path.splitext(archivo)[1]
+    nuevo_nombre = fecha_antigua.strftime('%Y-%m-%d %H-%M-%S') + os.path.splitext(archivo)[1]
     
     # Verificar si el nuevo nombre ya existe
     while os.path.exists(os.path.join(directorio, nuevo_nombre)):
         # Si el nuevo nombre ya existe, agregar un contador al nombre del archivo
         contador[nuevo_nombre] = contador.get(nuevo_nombre, 0) + 1
-        nuevo_nombre = fecha_antigua.strftime('%m-%d-%Y %H-%M-%S') + f" ({contador[nuevo_nombre]})" + os.path.splitext(archivo)[1]
+        nuevo_nombre = fecha_antigua.strftime('%Y-%m-%d %H-%M-%S') + f" ({contador[nuevo_nombre]})" + os.path.splitext(archivo)[1]
     
     # Renombrar el archivo con el nuevo nombre
     os.rename(ruta_completa, os.path.join(directorio, nuevo_nombre))
